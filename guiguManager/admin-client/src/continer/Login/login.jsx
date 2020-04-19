7c849940d7d79d8506bef7c7c93d2d13acaec87d
185207048 <18507048@qq.com>
2020-04-20 01:08:35 +0800

    2020年4月20日修改

---
+++ guiguManager/admin-client/src/continer/Login/login.jsx
@@ -0,0 +1,15 @@
+import React ,{Component} from 'react'
+import '../../assests/css/login.less'
+
+export default class Login extends Component{
+
+    render(){
+        return(
+            <div className='container login-body'>
+                <div className='login-header'>
+                    
+                </div>
+            </div>
+        )
+    }
+}
